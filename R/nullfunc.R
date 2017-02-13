1d7b2158deb22bc3e816c13cd25049d8fed795e0
Teo <teoboot2007@hotmail.com>
2017-02-13 00:49:54 +0000

    first commit of popPack

---
+++ R/nullfunc.R
@@ -0,0 +1,10 @@
+#' A not-useful-at-all function
+#'
+#' A not-useful-at-all function
+#' 
+#' This is a function which returns NULL. It is there just for the sake of having a function
+#' in the package.
+#'
+#' @return NULL
+#' 
+nullfunc <- function() NULL
